dfce9c2f637861fc02e6642c016926fe9eb31b6e
lijp19@lenovo.com <lijp19@lenovo.com>
2023-07-06 11:28:03 +0800

    tijiaoceshi

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,12 @@
 <template>
-    <div>
-    
-    </div>
-    </template>
-    
-    <script setup lang='ts'>
-    import { ref, reactive } from 'vue'
-    const ceshi = ()=>
-    {
-        console.log("算了")
+  <div></div>
+</template>
 
+<script setup lang="ts">
+import { ref, reactive } from "vue";
+const ceshi = () => {
+  console.log("算了");
+};
+</script>
 
-
-        
-    }
-    </script>
-    
-    <style scoped>
-    
-    </style>
+<style scoped></style>
