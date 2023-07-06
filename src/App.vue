e5c889f86be1557f8f6a8ffe192099805eb93b95
lijp19@lenovo.com <lijp19@lenovo.com>
2023-07-06 11:21:12 +0800

    提交测试

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-<div>
-
-</div>
-</template>
-
-<script setup lang='ts'>
-import { ref, reactive } from 'vue'
-const ceshi = ()=>
-{
-    console.log("算了")
-}
-</script>
-
-<style scoped>
-
-</style>
+    <div>
+    
+    </div>
+    </template>
+    
+    <script setup lang='ts'>
+    import { ref, reactive } from 'vue'
+    const ceshi = ()=>
+    {
+        console.log("算了")
+    }
+    </script>
+    
+    <style scoped>
+    
+    </style>
